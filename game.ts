3db4de93a951e08eb2f9c9a907b4b2326d2ec1b1
ItIsAlbin <83208543+ItIsAlbin@users.noreply.github.com>
2022-05-08 14:59:21 +0200

    Collision test added

--- game.ts
+++ game.ts
@@ -8,31 +8,71 @@ document.body.appendChild(app.view);
 
 app.renderer.resize(1200, 800);
 
-const sceneObjs: PhysicObject[] = [];
+const physicsObjs: PhysicObject[] = [];
 
-sceneObjs.push(
+physicsObjs.push(
   new Player({ x: 100, y: 100 }, 50, './assets/images/Yrgonaut.png')
 );
 
-sceneObjs.push(
-  new SpringCircle(
-    { x: 600, y: 400 },
-    50,
-    './assets/images/Yrgonaut.png',
-    0.05,
-    0.95
-  )
-);
+for (let i = 0; i < 10; i++) {
+  physicsObjs.push(
+    new SpringCircle(
+      { x: 600 + i * 10, y: 400 + Math.random() * 2 },
+      50,
+      './assets/images/Yrgonaut.png',
+      0.001,
+      0.99
+    )
+  );
+}
 
-sceneObjs.forEach((obj) => {
+physicsObjs.forEach((obj) => {
   app.stage.addChild(obj.sprite);
 });
 
-sceneObjs[0].addForce({ x: 1, y: 2 });
-sceneObjs[1].addForce({ x: 100, y: 100 });
+physicsObjs[0].addForce({ x: 1, y: 2 });
 app.ticker.add(() => {
-  sceneObjs.forEach((obj) => {
+  physicsObjs.forEach((obj) => {
     obj.checkBorders(0.6);
     obj.updatePosition();
   });
+  checkCollisions();
 });
+
+function checkCollisions(): void {
+  for (let i = 0; i < physicsObjs.length; i++) {
+    for (let j = 0; j < physicsObjs.length; j++) {
+      if (physicsObjs[i] === physicsObjs[j]) {
+        continue;
+      }
+      const betweenVector = getBetweenVector(
+        physicsObjs[i].position,
+        physicsObjs[j].position
+      );
+      const m = magnitude(betweenVector);
+      const collideDistance =
+        m - (physicsObjs[i].radius + physicsObjs[j].radius);
+      if (collideDistance < 0) {
+        const dirV = normalizeM(betweenVector, m);
+        physicsObjs[i].addForce({
+          x: dirV.x * collideDistance * 0.1,
+          y: dirV.y * collideDistance * 0.1,
+        });
+        physicsObjs[i].moveVector.x *= 0.96;
+        physicsObjs[i].moveVector.y *= 0.96;
+      }
+    }
+  }
+}
+
+function magnitude(v: Vector2): number {
+  return Math.sqrt(v.x * v.x + v.y * v.y);
+}
+
+function getBetweenVector(fromV: Vector2, toV: Vector2) {
+  return { x: toV.x - fromV.x, y: toV.y - fromV.y };
+}
+
+function normalizeM(v: Vector2, m: number): Vector2 {
+  return { x: v.x / m, y: v.y / m };
+}
